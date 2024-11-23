e303e3cc4f43f1561046c1eabb6db1d9063f12b8
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2024-11-22 22:58:48 -0600

    Initial commit: Scaffold Ruby gem to consume EasyBroker's stage API

---
+++ lib/easy_broker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "easy_broker/version"
+
+module EasyBroker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_easy_broker.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestEasyBroker < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::EasyBroker::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
