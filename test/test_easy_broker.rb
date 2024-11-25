f7bb2abe74ad5d1196a7eaac5b493a145b242cea
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2024-11-25 17:52:52 -0600

    Add tests for property client class

--- test/test_easy_broker.rb
+++ test/test_easy_broker.rb
@@ -3,11 +3,145 @@
 require "test_helper"
 
 class TestEasyBroker < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::EasyBroker::VERSION
+
+  def setup
+    @endpoint = "https://myapi.com:80/fakeapi"
+    @api_key = "123"
+    @timeout = 10
+    @client = EasyBroker::PropertyClient.new(@api_key, @endpoint, @timeout)
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_initialize
+    assert_equal @endpoint, @client.endpoint
+    assert_equal @api_key, @client.api_key
+    assert_equal @timeout, @client.timeout
+  end
+
+  def test_call_api_success
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 200, body: "")
+
+    result = @client.call_api()
+    assert result.is_a?(Net::HTTPSuccess)
+  end
+
+  def test_call_api_error
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 400, body: "")
+
+    exception = assert_raises(StandardError) do
+      @client.call_api()
+    end
+    assert_equal "Request failed due to status 400", exception.message
+  end
+
+  def test_call_api_retry
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 429, body: "")
+
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 429, body: "")
+
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 200, body: "")
+
+    result = @client.call_api()
+    assert result.is_a?(Net::HTTPSuccess)
+  end
+
+  def test_call_api_retry_exceeded
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 429, body: "")
+
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 429, body: "")
+
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 429, body: "")
+
+    exception = assert_raises(StandardError) do
+      @client.call_api()
+    end
+    assert_equal "Retries exceeded", exception.message
+  end
+
+  def test_print_paginated_content_error
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 200, body: "")
+
+    assert_raises(StandardError) do
+      @client.print_paginated_content
+    end
+  end
+
+  def test_print_paginated_success
+    page1 = {
+      "pagination" => {
+        "next_page" => "https://myapi.com:80/fakeapi?page=2"
+      },
+      "content" => [
+        {
+          "title" => "Title 1"
+        }
+      ]
+    }.to_json
+
+    page2 = {
+      "pagination" => {
+        "next_page" => "https://myapi.com:80/fakeapi?page=3"
+      },
+      "content" => [
+        {
+          "title" => "Title 2"
+        }
+      ]
+    }.to_json
+
+    page3 = {
+      "pagination" => {
+        "next_page" => ""
+      },
+      "content" => [
+        {
+          "title" => "Title 3"
+        }
+      ]
+    }.to_json
+
+    stub_request(:get, @endpoint)
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 200, body: page1)
+
+    stub_request(:get, "https://myapi.com:80/fakeapi?page=2")
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 200, body: page2)
+
+    stub_request(:get, "https://myapi.com:80/fakeapi?page=3")
+    .with(headers:{"X-Authorization" => @api_key})
+    .to_return(status: 200, body: page3)
+
+    $stdout = output = StringIO.new
+
+    @client.print_paginated_content
+
+    $stdout = STDOUT
+
+    assert_includes output.string, "Title 1"
+    assert_includes output.string, "Title 2"
+    assert_includes output.string, "Title 3"
+  end
+
+
+  def test_that_it_has_a_version_number
+    refute_nil ::EasyBroker::VERSION
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "easy_broker"
 
 require "minitest/autorun"
+require "net/http"
+require "webmock/minitest"
+require "json"
+require "stringio"
